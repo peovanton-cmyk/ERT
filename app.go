43ea45093a6e274f9154a38c3863b17dce3e6a65
Anton Petuhov <peov.anton@gmail.com>
2025-11-07 15:42:47 +0000

    add changes in file app.go

--- app.go
+++ app.go
@@ -7,13 +7,8 @@ import (
 )
 
 func main() {
-	//TODO #1 exchange type I not use parametr message use change in variable where I isset type
 	var i (I)
-	// var i (I) = I{
-	// 	message: Hello.Hello("This first tutorial"),
-	// }
 	i.message = Hello.Hello("This first tutorial")
-
 	i.Message()
 }
 
